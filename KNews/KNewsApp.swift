ebe8b68245884a824c3fcf38994add7f9e799ecf
Kelvin Javorski <kelvin.javorskisoares@gmail.com>
2024-05-14 22:54:58 -0300

    Initial Commit

---
+++ KNews/KNewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  KNewsApp.swift
+//  KNews
+//
+//  Created by Kelvin Javorski Soares on 14/05/24.
+//
+
+import SwiftUI
+
+@main
+struct KNewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
